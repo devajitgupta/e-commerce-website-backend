de1b34f9cbd8fb528dbe595d860406eaf6f7df79
devajitgupta <yourajitgupta@gmail.com>
2023-10-31 18:33:45 +0530

    first commit

--- src/common/schemas/products.schema.ts
+++ src/common/schemas/products.schema.ts
@@ -5,7 +5,7 @@ export type ProductsDocument = Products & Document;
 @Schema({ timestamps: true, collection: 'Products' })
 export class Products {
   @Prop()
-  title: string;
+  name: string;
 
   @Prop({ required: false })
   description: string;
@@ -14,12 +14,62 @@ export class Products {
   price: number;
 
   @Prop({ required: false })
+  images: [
+    {
+      public_id: {
+        type: string;
+      };
+      url: string;
+    },
+  ];
+  @Prop({
+    type: String, // Specify the data type as String
+    required: [false, 'Please enter a product category'],
+    enum: {
+      values: [
+        'Electronics',
+        'Cameras',
+        'Laptops',
+        'Accessories',
+        'Headphones',
+        'Sports',
+      ],
+    },
+  })
   category: string;
 
   @Prop({ required: false })
-  image: string;
+  seller: string;
 
   @Prop({ required: false })
-  stockQuantity: Number;
+  stock: number;
+
+  @Prop({
+    type: Number, // Specify the data type as Number
+    default: 0,
+  })
+  ratings: number;
+
+  @Prop({ required: false })
+  reviews: [
+    {
+      rating: {
+        type: Number;
+      };
+      comment: {
+        type: String; // Use 'String' instead of 'string'
+      };
+      createdAt: {
+        type: Date;
+        default: Date;
+      };
+    },
+  ];
+
+  @Prop({
+    type: Date, // Specify the data type as Number
+    default: 0,
+  })
+  createdAt: Date;
 }
 export const ProductsSchema = SchemaFactory.createForClass(Products);
